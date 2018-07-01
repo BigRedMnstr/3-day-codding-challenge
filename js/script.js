0d218760886976ebb7380c88b928cca307abef7c
BigRedMnstr <m-kucharski@wp.pl>
2018-07-01 19:22:03 +0200

    Added responsive behavior

--- js/script.js
+++ js/script.js
@@ -3,13 +3,13 @@ $('.slick-slider').slick({
     arrows: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToShow: 2,
+    slidesToScroll: 2,
     responsive: [{
         breakpoint: 1024,
         settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
+            slidesToShow: 2,
+            slidesToScroll: 2,
             infinite: true,
             dots: true,
             arrows: true
@@ -18,13 +18,15 @@ $('.slick-slider').slick({
         breakpoint: 600,
         settings: {
             slidesToShow: 2,
-            slidesToScroll: 2
+            slidesToScroll: 2,
+            dots: false
         }
     }, {
         breakpoint: 480,
         settings: {
             slidesToShow: 1,
-            slidesToScroll: 1
+            slidesToScroll: 1,
+            dots:false
         }
     }]
 });
