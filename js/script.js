f8c8d8552fba4cb1c227abce77c1dfb6f0ef5ef4
BigRedMnstr <m-kucharski@wp.pl>
2018-07-03 00:10:47 +0200

    Added jQuery scrollup

--- js/script.js
+++ js/script.js
@@ -26,7 +26,24 @@ $('.slick-slider').slick({
         settings: {
             slidesToShow: 1,
             slidesToScroll: 1,
-            dots:false
+            dots: false
         }
     }]
+});
+
+$(document).ready(function () {
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.scrollup').fadeIn();
+        } else {
+            $('.scrollup').fadeOut();
+        }
+    });
+    $('.scrollup').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+    });
 });
